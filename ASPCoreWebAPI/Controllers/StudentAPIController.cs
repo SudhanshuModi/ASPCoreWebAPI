3a6aed13c404bead0c3fa3874a848a65350397c4
SudhanshuModi <sudhanshu565868@gmail.com>
2024-01-12 15:10:07 +0530

    Add project files.

---
+++ ASPCoreWebAPI/Controllers/StudentAPIController.cs
@@ -0,0 +1,71 @@
+﻿using ASPCoreWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly StudentDBContext studentDB;
+
+        public StudentAPIController(StudentDBContext studentDB)
+        {
+            this.studentDB = studentDB;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            var data = await studentDB.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentById(int id)
+        {
+            var student = await studentDB.Students.FindAsync(id);
+            if(student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student std)
+        {
+            await studentDB.Students.AddAsync(std);
+            await studentDB.SaveChangesAsync();
+            return Ok(std);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>> UpdateStudent(int id, Student std)
+        {
+            if(id != std.Id)
+            {
+                return BadRequest();
+            }
+            studentDB.Entry(std).State = EntityState.Modified;
+            await studentDB.SaveChangesAsync();
+            return Ok(std);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(int id)
+        {
+            var std = await studentDB.Students.FindAsync(id);
+            if (std == null)
+            {
+                return NotFound();
+            }
+            studentDB.Remove(std);
+            await studentDB.SaveChangesAsync();
+            return Ok(std);
+        }
+
+    }
+}
---
+++ ASPCoreWebAPI/Models/StudentDBContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ASPCoreWebAPI.Models
+{
+    public partial class StudentDBContext : DbContext
+    {
+        public StudentDBContext()
+        {
+        }
+
+        public StudentDBContext(DbContextOptions<StudentDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.StudentGender).HasMaxLength(20);
+
+                entity.Property(e => e.StudentName).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
